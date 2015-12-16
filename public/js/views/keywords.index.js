b456d80793e2dbeaab7ad3ab7ee7f8998935181a
Aljo Victor Gabot <aljo.gabot@gmail.com>
2015-12-17 02:22:15 +0800

    Added Public Packages

---
+++ public/js/views/keywords.index.js
@@ -0,0 +1,91 @@
+function KeywordsIndex()
+{
+	this.construct();
+};
+
+KeywordsIndex.prototype = {
+
+	construct : function()
+	{
+		this.init_main_form();
+		this.init_events();
+	},
+
+	init_main_form : function()
+	{
+		var $self = this;
+
+		$( 'form[name=keywords-form]' ).submit(
+			function( $event )
+			{
+				$event.preventDefault();
+				var $form = $( this );
+
+				var $data = new FormData( $form[0] );
+
+				$FormMessageService.setElement( $form );
+
+				$http.postUpload( $form.attr( 'action' ), $data,
+					function( $json_response ) {
+						if( $json_response.success ) {
+							$BootstrapModalService.setContent( $json_response.content ).load(
+								function()
+								{
+									$self.init_multiply_form();
+								}
+							);
+							$FormMessageService.notify( 'This is an App where you can multiply your selected keywords ...' );
+						} else {
+							$FormMessageService.error( $json_response.message );
+						}
+					}
+				);
+			}
+		);
+	},
+
+	init_events : function()
+	{
+		$( '.upload' ).change(
+			function()
+			{
+				$( '#file_label' ).val( $( this ).val() );
+			}
+		);
+	},
+
+	init_multiply_form : function()
+	{
+		$( 'form[name=keywords-preview-form]' ).submit(
+			function( $event )
+			{
+				$event.preventDefault();
+				var $form = $( this );
+				var $data = $form.serialize();
+
+				$FormMessageService.setElement( $form );
+				$FormMessageService.notify( 'Processing ...' );
+
+				$http.post( $form.attr( 'action' ), $data,
+					function( $json_response ) {
+						if( $json_response.success ) {
+							$BootstrapModalService.setContent( $json_response.content ).load();
+						} else {
+							$FormMessageService.error( $json_response.message );
+						}
+					}
+				);
+			}
+		);
+	}
+
+};
+
+var $KeywordsIndex;
+
+$( document ).ready(
+	function()
+	{
+		$KeywordsIndex = new KeywordsIndex();
+	}
+);
