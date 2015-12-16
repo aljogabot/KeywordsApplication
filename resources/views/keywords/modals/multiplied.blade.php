e4e4f49e52cfb5cf6542f11fe6b4135b412490eb
Aljo Victor Gabot <aljo.gabot@gmail.com>
2015-12-17 04:45:47 +0800

    Move the Results Summary at the TOP

--- resources/views/keywords/modals/multiplied.blade.php
+++ resources/views/keywords/modals/multiplied.blade.php
@@ -8,13 +8,6 @@
         <div class="alert alert-info">
             <h4 style="margin-bottom: 3px;">Multiplied Keywords</h4>
         </div>
-        <ul>
-            @foreach( $keywords as $keyword )
-                <li style="margin-bottom: 3px;">
-                    <strong>{{ $keyword }}</strong>
-                </li>
-            @endForeach
-        </ul>
         <div class="bs-component">
             <ul class="list-group">
                 <li class="list-group-item">
@@ -31,6 +24,13 @@
                 </li>
             </ul>
         </div>
+        <ul>
+            @foreach( $keywords as $keyword )
+                <li style="margin-bottom: 3px;">
+                    <strong>{{ $keyword }}</strong>
+                </li>
+            @endForeach
+        </ul>
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
